c969798fdec237676e8fa3893e94ee392b1ec018
Tim Jacomb <tim.jacomb@hmcts.net>
2019-11-18 09:36:26 +0000

    Add README

---
+++ README.md
@@ -0,0 +1,9 @@
+## Inputs
+
+| Name | Description | Type | Default |
+|------|-------------|:----:|:-----:|
+| api\_mgmt\_name | Name of the api management, e.g. core-infra-demo | string | n/a |
+| api\_mgmt\_rg | Resource group that api management is in, e.g. core-infra-demo | string | n/a |
+| api\_name | Name of the API, e.g. bulkscanning | string | n/a |
+| api\_policy\_xml\_content | Content of the policy, in XML format | string | n/a |
+
--- variables.tf
+++ variables.tf
@@ -1,7 +1,15 @@
-variable "api_policy_xml_content" {}
+variable "api_policy_xml_content" {
+  description = "Content of the policy, in XML format"
+}
 
-variable "api_mgmt_rg" {}
+variable "api_mgmt_rg" {
+  description = "Resource group that api management is in, e.g. core-infra-demo"
+}
 
-variable "api_mgmt_name" {}
+variable "api_mgmt_name" {
+  description = "Name of the api management, e.g. core-infra-demo"
+}
 
-variable "api_name" {}
+variable "api_name" {
+  description = "Name of the API, e.g. bulkscanning"
+}
